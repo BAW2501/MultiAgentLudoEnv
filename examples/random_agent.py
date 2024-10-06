3f3171a1668e7e2881fd5d853d0dd287ba6ed68b
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2024-10-06 21:42:33 +0100

    updated notebook and random agent

--- examples/random_agent.py
+++ examples/random_agent.py
@@ -1,17 +1,17 @@
-# from Ludo.envs import LudoEnv
+from Ludo.envs import LudoEnv
 
-# if __name__ == "__main__":
-#     env = LudoEnv()
-#     env.reset(seed=42)
+if __name__ == "__main__":
+    env = LudoEnv()
+    env.reset(seed=42)
 
-#     for agent in env.agent_iter():
-#         observation, reward, termination, truncation, info = env.last()
+    for agent in env.agent_iter():
+        observation, reward, termination, truncation, info = env.last()
 
-#         if termination or truncation:
-#             print(f"Agent {agent} has reached the final square.")
-#             break
-#         # this is where you would insert your policy
-#         action = env.action_space(agent).sample()
-#         env.step(action)
-#         env.render()
-#     env.close()
+        if termination or truncation:
+            print(f"Agent {agent} has reached the final square.")
+            break
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample()
+        env.step(action)
+        env.render()
+    env.close()
